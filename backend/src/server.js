2a675166a1b106e74b3d271f1ccca8d77a88771d
Marcelo Barros <mhbarros99@gmail.com>
2019-08-07 00:52:20 -0300

    Cross-origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express  = require('express');
 const mongoose = require('mongoose');
-const rotas = require('./rotas');
+const cors     = require('cors');
+const rotas    = require('./rotas');
 
 mongoose.connect('mongodb+srv://mhbarros:mhbarros@cluster0-zkqfb.mongodb.net/tindev?retryWrites=true&w=majority',{useNewUrlParser:true});
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 server.use(rotas);
 
